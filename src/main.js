175e1477e40fb0bd3092e8b092b92f757453fb4b
VladKurluk <vlad11kurluk89@gmail.com>
2020-03-10 17:52:49 +0200

    Init

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'buefy/dist/buefy.css'
+import './assets/styles/main.scss'
+import { Input, Button } from 'buefy'
+
+Vue.use(Input)
+Vue.use(Button)
 
 Vue.config.productionTip = false
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <div class="columns">
+      <div class="column is-11">
+        <b-field label="Email"
+          type="is-primary"
+          message="This email is invalid">
+          <b-input type="email"
+            value="john@"
+            maxlength="30"></b-input>
+        </b-field>
+      </div>
+      <div class="column is-1 has-background-warning">
+        <b-button type="is-primary" @click="clickMe">Click Me</b-button>
+      </div>
+    </div>
   </div>
 </template>
