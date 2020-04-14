c9e67dc9917347077fa5e53485c3e3fd86a43ab8
Vladislav Kurluk <vlad11kurluk89@gmail.com>
2020-04-14 19:38:06 +0300

    Hot fix

--- Client/src/views/freelancehunt/Profile.vue
+++ Client/src/views/freelancehunt/Profile.vue
@@ -1,65 +1,67 @@
 <template>
 <div>
-  <div class="container">
-    <div class="columns">
-      <div class="column is-6">
-        <div v-if="data.length !== 0" class="card">
-          <div class="card-content">
-            <div class="media">
-              <div class="media-left">
-                <figure class="image is-48x48">
-                  <img :src="data.data.attributes.avatar.small.url" alt="Placeholder image">
-                </figure>
+  <div class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-6">
+          <div v-if="data.length !== 0" class="card">
+            <div class="card-content">
+              <div class="media">
+                <div class="media-left">
+                  <figure class="image is-48x48">
+                    <img :src="data.data.attributes.avatar.small.url" alt="Placeholder image">
+                  </figure>
+                </div>
+                <div class="media-content">
+                  <p class="title is-4">{{`${data.data.attributes.first_name} ${data.data.attributes.last_name}`}}</p>
+                  <p class="subtitle is-6">{{`${data.data.type}`}}</p>
+                  <!-- <p class="subtitle is-6"><a :href="data.data.links.self.web" target="_blank">Смотреть профиль на Freelancehunt</a></p> -->
+                </div>
               </div>
-              <div class="media-content">
-                <p class="title is-4">{{`${data.data.attributes.first_name} ${data.data.attributes.last_name}`}}</p>
-                <p class="subtitle is-6">{{`${data.data.type}`}}</p>
-                <!-- <p class="subtitle is-6"><a :href="data.data.links.self.web" target="_blank">Смотреть профиль на Freelancehunt</a></p> -->
-              </div>
-            </div>
 
-            <div class="content">
-              {{data.data.attributes.cv}}
-              <br>
-                <a>@bulmaio</a>
-                <a href="#">#css</a>
-                <a href="#">#responsive</a>
-              <br>
-              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+              <div class="content">
+                {{data.data.attributes.cv}}
+                <br>
+                  <a>@bulmaio</a>
+                  <a href="#">#css</a>
+                  <a href="#">#responsive</a>
+                <br>
+                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+              </div>
             </div>
           </div>
+          <b-loading v-else :is-full-page="isFullPage" :active.sync="isLoading" :can-cancel="false"></b-loading>
         </div>
-        <b-loading v-else :is-full-page="isFullPage" :active.sync="isLoading" :can-cancel="false"></b-loading>
-      </div>
 
-      <div class="column is-6">
-        <div v-if="data.length !== 0" class="card">
-          <div class="card-content">
-            <div class="media">
-              <div class="media-left">
-                <figure class="image is-48x48">
-                  <img :src="data.data.attributes.avatar.small.url" alt="Placeholder image">
-                </figure>
+        <div class="column is-6">
+          <div v-if="data.length !== 0" class="card">
+            <div class="card-content">
+              <div class="media">
+                <div class="media-left">
+                  <figure class="image is-48x48">
+                    <img :src="data.data.attributes.avatar.small.url" alt="Placeholder image">
+                  </figure>
+                </div>
+                <div class="media-content">
+                  <p class="title is-4">{{`${data.data.attributes.first_name} ${data.data.attributes.last_name}`}}</p>
+                  <p class="subtitle is-6">{{`${data.data.type}`}}</p>
+                  <!-- <p class="subtitle is-6"><a :href="data.data.links.self.web" target="_blank">Смотреть профиль на Freelancehunt</a></p> -->
+                </div>
               </div>
-              <div class="media-content">
-                <p class="title is-4">{{`${data.data.attributes.first_name} ${data.data.attributes.last_name}`}}</p>
-                <p class="subtitle is-6">{{`${data.data.type}`}}</p>
-                <!-- <p class="subtitle is-6"><a :href="data.data.links.self.web" target="_blank">Смотреть профиль на Freelancehunt</a></p> -->
-              </div>
-            </div>
 
-            <div class="content">
-              {{data.data.attributes.cv}}
-              <br>
-                <a>@bulmaio</a>
-                <a href="#">#css</a>
-                <a href="#">#responsive</a>
-              <br>
-              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+              <div class="content">
+                {{data.data.attributes.cv}}
+                <br>
+                  <a>@bulmaio</a>
+                  <a href="#">#css</a>
+                  <a href="#">#responsive</a>
+                <br>
+                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+              </div>
             </div>
           </div>
+          <b-loading v-else :is-full-page="isFullPage" :active.sync="isLoading" :can-cancel="false"></b-loading>
         </div>
-        <b-loading v-else :is-full-page="isFullPage" :active.sync="isLoading" :can-cancel="false"></b-loading>
       </div>
     </div>
   </div>
@@ -77,13 +79,13 @@ export default {
     isFullPage: true
   }),
   methods: {
-    getProfileData () {
-      axiosInst.get('curent_profile')
+    async getProfileData () {
+      await axiosInst.get('curent_profile')
         .then(response => (this.data = response.data))
     }
   },
-  created () {
-    this.getProfileData()
+  async created () {
+    await this.getProfileData()
     this.isLoading = !this.isLoading
   }
 }
--- Client/src/views/parser/ParsingList.vue
+++ Client/src/views/parser/ParsingList.vue
@@ -5,7 +5,7 @@
         <div v-if="data.length !== 0" class="card">
           {{ data }}
         </div>
-        <b-loading v-else :is-full-page="isFullPage" :active.sync="isLoading" :can-cancel="false"></b-loading>
+        <b-loading v-else :is-full-page="isFullPage" :active="isLoading" :can-cancel="false"></b-loading>
       </div>
     </div>
   </div>
@@ -22,13 +22,13 @@ export default {
     isFullPage: true
   }),
   methods: {
-    getProfileData () {
-      axiosParserInst.get('parser')
+    async getProfileData () {
+      await axiosParserInst.get('parser')
         .then(response => (this.data = response.data))
     }
   },
-  created () {
-    this.getProfileData()
+  async created () {
+    await this.getProfileData()
     this.isLoading = !this.isLoading
   }
 }
