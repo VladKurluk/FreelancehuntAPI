4a801925b96bb9a3b60dec6729179b0c68e91c9c
Vladislav Kurluk <vlad11kurluk89@gmail.com>
2020-04-22 22:56:12 +0300

    Increased request timeout

--- Client/src/utils/http.js
+++ Client/src/utils/http.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const axiosInst = axios.create({
   baseURL: 'http://127.0.0.1:5000/api/v1/',
-  timeout: 1000
+  timeout: 5000
 })
 
 const axiosParserInst = axios.create({
