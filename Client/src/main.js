947b2676c688405c09f3a7d63d3a54d0b9f6c57b
Vladislav Kurluk <vlad11kurluk89@gmail.com>
2020-05-08 23:51:40 +0300

    Registered all Buefy components in my app

--- Client/src/main.js
+++ Client/src/main.js
@@ -5,16 +5,22 @@ import store from './store'
 
 import 'buefy/dist/buefy.css'
 import './assets/styles/main.scss'
-import { Input, Button, Loading } from 'buefy'
+// import { Input, Button, Loading, Tag, Field, Select, Pagination } from 'buefy'
+import Buefy from 'buefy'
 // Компонент для Сайдбар меню
 import VueSidebarMenu from 'vue-sidebar-menu'
 import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
 import dateFilter from '@/filters/date.filter'
 
-Vue.use(Input)
-Vue.use(Button)
-Vue.use(Loading)
+// Vue.use(Input)
+// Vue.use(Field)
+// Vue.use(Select)
+// Vue.use(Button)
+// Vue.use(Loading)
+// Vue.use(Tag)
+// Vue.use(Pagination)
 Vue.use(VueSidebarMenu)
+Vue.use(Buefy)
 Vue.filter('time', dateFilter)
 
 Vue.config.productionTip = false
